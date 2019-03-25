531b0b2c05d14b3bd2ee82fd550f6044312650dc
Robb Shecter <robb@public.law>
2019-03-25 13:57:15 -0700

    refactor: test DRY

--- spec/requests/frames_spec.rb
+++ spec/requests/frames_spec.rb
@@ -3,17 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe 'Frames', type: :request do
-  let(:headers) { { 'ACCEPT' => 'application/json' } }
   let(:new_game) { Game.create! }
 
   describe 'GET /games/:game_id/frames' do
     it 'returns success' do
-      get "/games/#{new_game.id}/frames", headers: headers
+      get "/games/#{new_game.id}/frames"
       expect(response).to have_http_status(200)
     end
 
     it 'returns content-type json' do
-      get "/games/#{new_game.id}/frames", headers: headers
+      get "/games/#{new_game.id}/frames"
       expect(response.content_type).to eq('application/json')
     end
 
