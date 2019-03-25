6905d5f6231c70930be6392b7296fedb2bb5ff72
Robb Shecter <robb@public.law>
2019-03-25 02:49:33 -0700

    passing test for /games content

--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -3,19 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Games', type: :request do
+  fixtures :games
+
   describe 'GET /games' do
     let(:headers) { { 'ACCEPT' => 'application/json' } }
 
     it 'is successful' do
       get '/games', headers: headers
-
       expect(response).to have_http_status(200)
     end
 
     it 'returns content-type json' do
       get '/games', headers: headers
-
       expect(response.content_type).to eq('application/json')
+    end
+
+    it 'returns an array of game objects' do
+      get '/games', headers: headers
+      expect(JSON.parse(response.body).first['id']).to eq(games(:new_game).id)
     end
   end
 end
