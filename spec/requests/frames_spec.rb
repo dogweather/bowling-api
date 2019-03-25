7dc72895c591a43b6432e34dd50349f884faf1ae
Robb Shecter <robb@public.law>
2019-03-25 13:51:50 -0700

    fix: remove fixture

--- spec/requests/frames_spec.rb
+++ spec/requests/frames_spec.rb
@@ -3,23 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Frames', type: :request do
-  fixtures :games
   let(:headers) { { 'ACCEPT' => 'application/json' } }
+  let(:new_game) { Game.create! }
 
   describe 'GET /games/:game_id/frames' do
     it 'returns success' do
-      get "/games/#{games(:new_game).id}/frames", headers: headers
+      get "/games/#{new_game.id}/frames", headers: headers
       expect(response).to have_http_status(200)
     end
 
     it 'returns content-type json' do
-      get "/games/#{games(:new_game).id}/frames", headers: headers
+      get "/games/#{new_game.id}/frames", headers: headers
       expect(response.content_type).to eq('application/json')
     end
 
-    it 'returns an empty list for a new game' do
-      get "/games/#{games(:new_game).id}/frames"
-      expect(response.body).to eq('[]')
+    it 'returns a list of ten frames for a new game' do
+      get "/games/#{new_game.id}/frames"
+      frame_count = JSON.parse(response.body).size
+      expect(frame_count).to eq(10)
     end
   end
 end
