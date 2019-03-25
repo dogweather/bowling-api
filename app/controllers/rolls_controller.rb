92e5d2a92e1988d96013b83af8a38660c9856401
Robb Shecter <robb@public.law>
2019-03-25 15:19:53 -0700

    refactor: DRY

--- app/controllers/rolls_controller.rb
+++ app/controllers/rolls_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class RollsController < ApplicationController
+  before_action :set_frame, only: %i[show create]
   before_action :set_roll, only: [:show]
 
   # GET /rolls
@@ -16,21 +17,16 @@ class RollsController < ApplicationController
 
   # POST /rolls
   def create
-    frame = Frame.find_by!(
-      game_id: params.fetch(:game_id),
-      number:  params.fetch(:frame_id)
-    )
-
     @roll = Roll.new(
-      frame_id: frame.id,
+      frame_id: @frame.id,
       score:    params.fetch(:score),
-      number:   frame.next_roll_number
+      number:   @frame.next_roll_number
     )
 
     if @roll.save
       url = game_frame_roll_path(
         game_id:  params.fetch(:game_id),
-        frame_id: params.fetch(:frame_id),
+        frame_id: @frame.number,
         id:       @roll.number
       )
       render json: @roll, status: :created, location: url
@@ -44,11 +40,13 @@ class RollsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_roll
     num = params[:id]
-    frame = Frame.find_by!(
+    @roll = Roll.find_by!(number: num, frame_id: @frame.id)
+  end
+
+  def set_frame
+    @frame = Frame.find_by!(
       game_id: params.fetch(:game_id),
       number:  params.fetch(:frame_id)
     )
-
-    @roll = Roll.find_by!(number: num, frame_id: frame.id)
   end
 end
