8d81387f65f39e8b0e41cac072a24ecaec7921ce
Robb Shecter <robb@public.law>
2019-03-24 20:24:35 -0700

    failing test: score gutterballs

--- spec/lib/bowling_spec.rb
+++ spec/lib/bowling_spec.rb
@@ -4,4 +4,15 @@ require 'spec_helper'
 require 'bowling'
 
 RSpec.describe Bowling do
+  # A simple Duck-type Frame which responds to #rolls,
+  # returning an array of two or three integers representing
+  # the number of pins hit on the rolls.
+  Frame = Struct.new('Frame', :rolls, keyword_init: true)
+
+  describe '#score' do
+    all_gutterballs = []
+    10.times { all_gutterballs << Frame.new(rolls: [0, 0]) }
+
+    expect(Bowling.score(for_frames: all_gutterballs)).to eq(0)
+  end
 end
