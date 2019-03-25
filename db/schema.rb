46de2c64a8967cb79ee9cd636fd0d1dafb881920
Robb Shecter <robb@public.law>
2019-03-25 14:05:12 -0700

    add missing null constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_25_120537) do
+ActiveRecord::Schema.define(version: 2019_03_25_205915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "frames", force: :cascade do |t|
-    t.integer "number"
-    t.bigint "game_id"
+    t.integer "number", null: false
+    t.bigint "game_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_frames_on_game_id"
