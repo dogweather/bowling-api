e2b437bbac1860742fcfcb63287cae0d58e048fb
Robb Shecter <robb@public.law>
2019-03-25 00:40:39 -0700

    Scoring (#4)
    * First bowling spec and lib
    
    * failing test: score gutterballs
    
    * better failing test
    
    * passing test
    
    * refactor: test
    
    * failing test for one-pointer
    
    * passing test
    
    * passing test for many pins hit
    
    * fix: naming
    
    * failing test for rolling a spare
    
    * spare test passes
    
    * refactor
    
    * refactor
    
    * refactor: naming
    
    * passing test for strike
    
    * failing test for perfect game
    
    * two failing tests
    
    * all tests pass
    
    * cleanup
    
    * cleanup
    
    * passing test for all spares
    
    * passing test for spare in the 9th frame
    
    * passing test for strike in the 9th frame
    
    * refactor: extract #all_rolls
    
    * extra safety: next_two_roles
    
    * refactor: extract #bonus
    
    * refactor: clarity
    
    * refactor: docs and naming
    
    * confirmatory unit test for #take_all_by_3
    
    * doc

---
+++ lib/bowling.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+# Ten-pin Bowling algorithms and logic
+module Bowling
+  module_function
+
+  # The public function to compute the score of
+  # a ten-pin bowling game.
+  #
+  # Add the basic score of 1 point per pin knocked down
+  # to the additional points given for strikes and spares.
+  #
+  # @param for_frames [Array<#rolls>] An Array of objects
+  #        which respond to #rolls, which returns an Array of
+  #        integers representing how many pins were knocked
+  #        down by a roll.
+  # @return [Integer]
+  def score(for_frames:)
+    simple_points(for_frames) + total_bonus(for_frames)
+  end
+
+  # Private
+
+  # The total number of pins knocked down in the game.
+  # Each pin equals one point.
+  #
+  # @return [Integer]
+  def simple_points(frames)
+    all_rolls(frames).sum
+  end
+
+  # The total additional points given due to rolling
+  # a spare or strike.
+  #
+  # Algorithm: Iterate through each frame, calculating
+  # the spare/strike bonus due. Do this by considering three
+  # frames at a time: the current frame plus the next two
+  # which follow. The player may have rolled many strikes, and
+  # so, up to two following frames may need to be consulted.
+  #
+  # @return [Integer]
+  def total_bonus(frames)
+    # All except the 10th (last) frame - it doesn't get a bonus
+    frame_tuples_to_check = take_all_by_3(frames)[0...-1]
+    frame_tuples_to_check.map { |tuple| bonus(tuple) }
+                         .sum
+  end
+
+  # Calculate a frame's bonus by the rules of ten-pin,
+  # which require looking ahead one or two frames:
+  # A spare gets the bonus of the next roll. A strike
+  # gets the bonus of the next two rolls.
+  #
+  # @param frame_tuple [Array<#rolls>] an Array of three frames
+  # @return [Integer]
+  def bonus(frame_tuple)
+    current_frame, next1, next2 = frame_tuple
+
+    next_two_rolls = all_rolls([next1, next2]).slice(0, 2)
+
+    if spare?(current_frame)
+      next_two_rolls.first
+    elsif strike?(current_frame)
+      next_two_rolls.sum
+    else
+      0
+    end
+  end
+
+  # Be forgiving of nils in the input,
+  # and return an array of the counts of
+  # pins knocked down.
+  #
+  # @return [Array<Integer>]
+  def all_rolls(frames)
+    frames.compact
+          .map(&:rolls)
+          .flatten
+  end
+
+  def spare?(frame)
+    frame.rolls.count == 2 && frame.rolls.sum == 10
+  end
+
+  def strike?(frame)
+    frame.rolls.count == 1 && frame.rolls.first == 10
+  end
+
+  # Return an array of arrays consisting of each element
+  # followed by its two successors or `nil` if none.
+  #
+  # @example
+  #
+  #   ['a', 'b', 'c'] => [['a', 'b', 'c'], ['b', 'c', nil], ['c', nil, nil]]
+  #
+  # @param ary [Array]
+  # @return [Array<Array>]
+  def take_all_by_3(ary)
+    ary.map.with_index { |x, i| [x, ary[i + 1], ary[i + 2]] }
+  end
+end
---
+++ spec/lib/bowling_spec.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'bowling'
+
+RSpec.describe Bowling do
+  # A simple Duck-type Frame which responds to #rolls,
+  # returning an array of two or three integers representing
+  # the number of pins hit on the rolls.
+  Frame = Struct.new('Frame', :rolls, keyword_init: true)
+
+  describe '#score' do
+    let(:spare) { Frame.new(rolls: [1, 9]) }
+    let(:strike) { Frame.new(rolls: [10]) }
+
+    it 'returns 0 for all gutter-balls' do
+      all_gutterballs = Array.new(10, Frame.new(rolls: [0, 0]))
+
+      expect(Bowling.score(for_frames: all_gutterballs)).to eq(0)
+    end
+
+    it 'returns the sum of rolls (one pointer rolls)' do
+      one_pointer_rolls = Array.new(10, Frame.new(rolls: [1, 1]))
+
+      expect(Bowling.score(for_frames: one_pointer_rolls)).to eq(20)
+    end
+
+    it 'handles a spare correctly' do
+      frames = [spare] + Array.new(9, Frame.new(rolls: [3, 4]))
+      expect(Bowling.score(for_frames: frames)).to eq(76)
+    end
+
+    it 'handles a strike correctly' do
+      frames = [strike] + Array.new(9, Frame.new(rolls: [3, 4]))
+      expect(Bowling.score(for_frames: frames)).to eq(80)
+    end
+
+    it 'returns 300 for a perfect game' do
+      perfect_game = Array.new(9, strike) + [Frame.new(rolls: [10, 10, 10])]
+      expect(Bowling.score(for_frames: perfect_game)).to eq(300)
+    end
+
+    it 'correctly calculates all spares' do
+      game = Array.new(9, Frame.new(rolls: [9, 1])) +
+             [Frame.new(rolls: [9, 1, 9])]
+      expect(Bowling.score(for_frames: game)).to eq(190)
+    end
+
+    it 'correctly handles a spare in the 9th frame' do
+      game = [
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 9]),
+        Frame.new(rolls: [1, 2])
+      ]
+      expect(Bowling.score(for_frames: game)).to eq(38)
+    end
+
+    it 'correctly handles a strike in the 9th frame' do
+      game = [
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [1, 2]),
+        Frame.new(rolls: [10]),
+        Frame.new(rolls: [1, 2])
+      ]
+      expect(Bowling.score(for_frames: game)).to eq(40)
+    end
+  end
+
+  describe '#take_all_by_3' do
+    it 'produces a 3-tuple for every element, including nils if needed' do
+      input = [1, 2, 3, 4, 5]
+      expected = [[1, 2, 3], [2, 3, 4], [3, 4, 5], [4, 5, nil], [5, nil, nil]]
+      expect(Bowling.take_all_by_3(input)).to eq(expected)
+    end
+  end
+end
