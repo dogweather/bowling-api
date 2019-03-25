f2a814ff119023d4f6cbd86b507004abe00d599a
Robb Shecter <robb@public.law>
2019-03-25 02:32:40 -0700

    failing test for frames req spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :frames
-  resources :games, only: %i[index show create destroy]
+  resources :games, only: %i[index show create destroy] do
+    resources :frames, only: %i[index show update]
+  end
 end
--- spec/requests/frames_spec.rb
+++ spec/requests/frames_spec.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Frames", type: :request do
-  describe "GET /frames" do
-    it "works! (now write some real specs)" do
-      get frames_path
+RSpec.describe 'Frames', type: :request do
+  describe 'GET /games/:game_id/frames' do
+    it 'returns success' do
+      game = Game.create!
+
+      get "/games/#{game.id}/frames"
       expect(response).to have_http_status(200)
     end
   end
